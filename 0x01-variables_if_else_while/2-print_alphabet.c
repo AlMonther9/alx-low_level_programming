d2e4ac8db50187184ebb83427156aba553280825
AlMonther9 <ealmonzer667@gmail.com>
2023-08-11 15:09:13 -0700

    perfect lines

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <srdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: a program that prints the alphabet in lowercase
+ *
+ * Return: Always (0) (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
