79d126f0ef2904dc8224d999050531bcfcdf666d
AlMonther9 <ealmonzer667@gmail.com>
2023-08-16 05:07:53 -0700

    noice

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,9 +5,11 @@
  *
  * @c: takes input from other functions.
  *
- * Return: (1) is c if true else (0)
+ * Return: (1) is c if true
+ *	otherwise (0)
 */
-int _islpha(int c)
+
+int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
