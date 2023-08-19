60100972a2df39a1ab5cc6c7bdc88fe271eaf0e5
AlMonther9 <ealmonzer667@gmail.com>
2023-08-18 17:31:30 -0700

    sleep

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ *
+ * @n: input number
+*/
+
+void print_line(int n)
+{
+
+	int lnchr;
+
+	if (n <= 0)
+		_putchar('\n')
+	else
+	{
+		for (lnchr = 1; lnchr <= n; lnchr++)
+			_putchar('_')
+				_putchar('\n');
+	}
+}
