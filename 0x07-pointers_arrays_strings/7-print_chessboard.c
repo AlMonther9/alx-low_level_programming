bb13b6e1de39c4b03ed587f316e79d4173f0fc8c
AlMonther9 <ealmonzer667@gmail.com>
2023-08-29 06:33:01 -0700

    ?

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_chessboard - a function that prints the chessboard
+ * @a: rows
+ * Return: Nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, x;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (x = 0; x < 0; x++)
+		{
+			_putcahr(a[i][x]);
+		}
+		_putchar('\n');
+
+	}
+}
