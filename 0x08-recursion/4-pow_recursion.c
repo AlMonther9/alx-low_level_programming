be419f62c28311304988305cb69d03e0c99d2f98
AlMonther9 <ealmonzer667@gmail.com>
2023-08-30 12:17:13 -0700

    noice

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * pow_recursion - a function that returns the value of power.
+ * @x: input 1
+ * @y: input 2
+ * Return: -1 if y is lower thatn 0, else retrun reasult
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
